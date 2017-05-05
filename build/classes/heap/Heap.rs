767a86f817d37c0abe6ab7ec532a8a7ddaaefa13
rajesh <rajeshghosh4007@gmail.com>
2017-05-05 13:33:27 +0200

    heap with insert delete update

---
+++ build/classes/heap/Heap.rs
@@ -0,0 +1,2 @@
+heap.HeapApp
+heap.Heap
---
+++ src/heap/Node.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heap;
+
+/**
+ *
+ * @author rajesh
+ */
+public class Node {
+    private int iData;             // data item (key)
+// -------------------------------------------------------------
+   public Node(int key)           // constructor
+      { iData = key; }
+// -------------------------------------------------------------
+   public int getKey()
+      { return iData; }
+// -------------------------------------------------------------
+   public void setKey(int id)
+      { iData = id; }
+}
